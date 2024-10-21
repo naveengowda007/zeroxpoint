ad4f09e0182e36c0853a42360522ff8b659c043b
Sandman0083 <anirbanbhattacharya@amceducation.in>
2024-10-21 11:25:21 +0530

    Update db.js

--- Backend/zconfig/db.js
+++ Backend/zconfig/db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "db",
   user: "root",
-  password: "zeroxpoint@1234",
+  password: "my-secret-pw",
   database: "zeroxpoint",
 });
 
